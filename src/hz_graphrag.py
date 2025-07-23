958cf64348da170946fddf215ea26005f25b14c5
zhoutongsheng <zhoutongsheng@haizhi.com>
2025-07-23 19:19:18 +0800

    update

--- src/hz_graphrag.py
+++ src/hz_graphrag.py
@@ -327,7 +327,7 @@ async def search(request: Request):
 
 def main():
     api_workers = int(os.environ.get('API_WORKERS', 1))
-    uvicorn.run("hz_graphrag:app", host="0.0.0.0", port=12085, reload=False, workers=api_workers)
+    uvicorn.run("hz_graphrag:app", host="0.0.0.0", port=8050, reload=False, workers=api_workers)
 
 
 if __name__ == "__main__":
